259e228acc2270b3b2eeebaf009f7514f038a667
Chen Keinan <Hen.Keinan@gmail.com>
2021-07-27 09:42:08 +0300

    fix: update report output

--- internal/reports/processor.go
+++ internal/reports/processor.go
@@ -10,11 +10,16 @@ func GenerateAuditReport(adtsReport []*models.AuditBench) *uitable.Table {
 	table := uitable.New()
 	for _, failedAudit := range adtsReport {
 		table.MaxColWidth = 100
+		status := "Failed"
+		if failedAudit.NonApplicable {
+			status = "Warn"
+		}
 		table.Wrap = true // wrap columns
 		table.AddRow("--------------", "-------------------------------------------------------------------------------------------")
+		table.AddRow("Status:", status)
 		table.AddRow("Name:", failedAudit.Name)
 		table.AddRow("Description:", failedAudit.Description)
-		table.AddRow("Impact:", failedAudit.Impact)
+		table.AddRow("Audit:", failedAudit.AuditCommand)
 		table.AddRow("Remediation:", failedAudit.Remediation)
 		table.AddRow("References:", failedAudit.References)
 		table.AddRow("") // blank
