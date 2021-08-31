fce59aa821d091c22f790836d92db0040a094029
Chen Keinan <Hen.Keinan@gmail.com>
2021-08-31 10:07:22 +0300

    fix: add user plugin pkg

--- internal/bplugin/model.go
+++
@@ -1,8 +0,0 @@
-package bplugin
-
-import "plugin"
-
-//LxdBenchAuditResultHook hold the plugin symbol for Lxd bench audit result Hook
-type LxdBenchAuditResultHook struct {
-	Plugins []plugin.Symbol
-}
